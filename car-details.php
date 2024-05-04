8a6c7d2de9c5d00fa26c7e9346f41780593ccfce
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2024-05-04 13:53:11 +0800

    Add renting feature

--- car-details.php
+++ car-details.php
@@ -1,18 +1,59 @@
 <?php
 require_once "assets/component/header.php";
 require_once "functions/get-cars.php";
+
 if (isset($_GET['car_id'])) {
   $car_id = htmlspecialchars($_GET['car_id']);
   $db = new OracleDB();
   $result = getCarDetails($car_id, $db);
-  if(empty($result)){
+  if (empty($result)) {
     header("Location: /drivesation/car-list.php");
   }
-  echo "<pre>";
-  print_r($result);
-  echo "<pre>";
 } else {
   throw new Exception("Error: No Parameters");
+}
+
+if (session_status() == PHP_SESSION_NONE) {
+  session_start();
+}
+$userId = $_SESSION['user_id'];
+$db = new OracleDB();
+if (!$db->isConnected()) {
+  die("Database connection failed");
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (isset(
+    $_POST['pick_up_time'],
+    $_POST['rent_date_from'],
+    $_POST['rent_date_to'],
+  )) {
+    try {
+      $pick_up_time = $_POST['pick_up_time'];
+      $rent_date_from = $_POST['rent_date_from'];
+      $rent_date_to = $_POST['rent_date_to'];
+
+      $sql = "INSERT INTO Rent (rent_id, user_id, owner_id, car_id, pick_up_time, rent_date_from, rent_date_to, status, transaction_date) VALUES (rent_seq.NEXTVAL, :user_id, :owner_id, :car_id, :pick_up_time, TO_DATE(:rent_date_from, 'YYYY-MM-DD'), TO_DATE(:rent_date_to, 'YYYY-MM-DD'), 0, SYSDATE)";
+
+      $data = [
+        ':user_id' => $userId,
+        ':owner_id' => $result['OWNER_ID'],
+        ':car_id' => $result['CAR_ID'],
+        ':pick_up_time' => $pick_up_time,
+        ':rent_date_from' => $rent_date_from,
+        ':rent_date_to' => $rent_date_to,
+      ];
+
+      $db->executeQuery($sql, $data);
+
+      echo "<p>Rent successfully!</p>";
+      oci_free_statement($stid);
+    } catch (Exception $e) {
+      throw new Exception("EROR", $e->getMessage());
+    }
+  } else {
+    throw new Exception("Input required fields");
+  }
 }
 ?>
 
@@ -29,19 +70,37 @@ if (isset($_GET['car_id'])) {
 
 <body>
   <main>
-    <!-- <?php foreach ($result as $car) : ?>
-      <div class="card">
-        <img src="<?= $car['FILE_LINK']; ?>" alt="<?= $car['CAR_TITLE'] ?>">
-        <h1><?= htmlspecialchars($car['CAR_TITLE']); ?></h1>
-        <p><?= htmlspecialchars($car['CAR_TYPE']) ?></p>
-        <p><?= htmlspecialchars($car['SEAT_CAPACITY']) ?></p>
-        <p><?= "₱" . htmlspecialchars(number_format($car['AMOUNT'])) . "/ day" ?></p>
-        <a href="car-details.php?car_id=<?= urlencode($car['CAR_ID']) ?>">Rent</a>
+    <div class="card">
+      <img src="<?= $result['FILE_LINK']; ?>" alt="<?= $result['CAR_TITLE'] ?>">
+      <h1><?= htmlspecialchars($result['CAR_TITLE']); ?></h1>
+      <p><?= htmlspecialchars($result['CAR_TYPE']) ?></p>
+      <p><?= htmlspecialchars($result['SEAT_CAPACITY']) ?></p>
+      <p><?= htmlspecialchars($result['CAR_MODEL']) ?></p>
+      <p><?= htmlspecialchars($result['CAR_COLOR']) ?></p>
+      <p><?= htmlspecialchars($result['PLATE_NUMBER']) ?></p>
+      <p><?= htmlspecialchars($result['GAS_TYPE']) ?></p>
+      <p><?= htmlspecialchars($result['CAR_DESCRIPTION']) ?></p>
+      <p><?= htmlspecialchars($result['OWNER_NAME']) ?></p>
+      <p><?= htmlspecialchars($result['CONTACT_NUMBER']) ?></p>
+      <p><?= htmlspecialchars($result['EMAIL_ADDRESS']) ?></p>
+      <p><?= "₱" . htmlspecialchars(number_format($result['AMOUNT'])) . "/ day" ?></p>
+
+      <div>
+        <form method="POST">
+          <label for="pick_up_time">Pick up Time</label>
+          <input id="pick_up_time" name="pick_up_time" type="time">
+
+          <label for="rent_date_from">Pick up Date</label>
+          <input id="rent_date_from" name="rent_date_from" type="date">
+
+          <label for="rent_date_to">Return Date</label>
+          <input id="rent_date_to" name="rent_date_to" type="date">
+
+          <button type="submit">Rent a Car</button>
+        </form>
       </div>
-    <?php endforeach; ?> -->
-
   </main>
-
+  result
 
 </body>
 
--- functions/get-cars.php
+++ functions/get-cars.php
@@ -2,7 +2,7 @@
 function getAllCars($db)
 {
   try {
-    $sql = "SELECT c.car_id, c.car_title, c.car_type, c.seat_capacity, c.amount, d.file_link from Car c JOIN \"DOCUMENT\" d ON c.car_Id = d.car_id WHERE d.document_type = 'car_image'";
+    $sql = "SELECT c.car_id, c.car_title, c.car_type, c.seat_capacity, c.amount, d.file_link from Car c JOIN \"DOCUMENT\" d ON c.car_Id = d.car_id WHERE d.document_type = 'car_image' AND c.availability_status=1 AND c.status=1";
     $stid = $db->executeQuery($sql);
     $result = $db->fetchAll($stid);
     if ($result && count($result) > 0) {
@@ -18,7 +18,10 @@ function getAllCars($db)
 function getCarDetails($carId, $db)
 {
   try {
-    $sql = "SELECT c.*, d.file_link from Car c JOIN \"DOCUMENT\" d ON c.car_Id = d.car_id WHERE d.document_type = 'car_image' AND c.car_id = :car_id";
+    $sql = "SELECT c.*, NVL(u.first_name, '') || ' ' || NVL(u.last_name, '') as owner_name, u.contact_number, u.email_address, d.file_link from Car c
+    JOIN \"DOCUMENT\" d ON c.car_Id = d.car_id
+    JOIN \"USER\" u ON c.owner_id = u.user_id
+    WHERE d.document_type = 'car_image' AND c.car_id = :car_id";
 
     $data = [":car_id" => $carId];
 
