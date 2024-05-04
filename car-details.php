c4dc11a28da948d2b76c3fa237c467790770cdde
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2024-05-04 11:27:53 +0800

    Add car details

--- car-details.php
+++ car-details.php
@@ -1,12 +1,20 @@
 <?php
 require_once "assets/component/header.php";
 require_once "functions/get-cars.php";
-$db = new OracleDB();
-$result = getAllCars($db);
+if (isset($_GET['car_id'])) {
+  $car_id = htmlspecialchars($_GET['car_id']);
+  $db = new OracleDB();
+  $result = getCarDetails($car_id, $db);
+  if(empty($result)){
+    header("Location: /drivesation/car-list.php");
+  }
+  echo "<pre>";
+  print_r($result);
+  echo "<pre>";
+} else {
+  throw new Exception("Error: No Parameters");
+}
 ?>
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -14,24 +22,23 @@ $result = getAllCars($db);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Car List</title>
+  <title>Car Details</title>
   <link rel="stylesheet" href="assets/styles/user/layout.css">
-  <link rel="stylesheet" href="assets/styles/user/car-list.css">
-  <link rel="stylesheet" href="assets/styles/component/button.css">
+  <link rel="stylesheet" href="assets/styles/user/car-details.css">
 </head>
 
 <body>
   <main>
-    <?php foreach ($result as $car) : ?>
+    <!-- <?php foreach ($result as $car) : ?>
       <div class="card">
         <img src="<?= $car['FILE_LINK']; ?>" alt="<?= $car['CAR_TITLE'] ?>">
         <h1><?= htmlspecialchars($car['CAR_TITLE']); ?></h1>
         <p><?= htmlspecialchars($car['CAR_TYPE']) ?></p>
         <p><?= htmlspecialchars($car['SEAT_CAPACITY']) ?></p>
         <p><?= "₱" . htmlspecialchars(number_format($car['AMOUNT'])) . "/ day" ?></p>
-        <a id="rent-button" href="car-details.php?car_id=<?= urlencode($car['CAR_ID']) ?>">Rent</a>
+        <a href="car-details.php?car_id=<?= urlencode($car['CAR_ID']) ?>">Rent</a>
       </div>
-    <?php endforeach; ?>
+    <?php endforeach; ?> -->
 
   </main>
 
--- functions/get-cars.php
+++ functions/get-cars.php
@@ -2,7 +2,7 @@
 function getAllCars($db)
 {
   try {
-    $sql = "SELECT * FROM Car";
+    $sql = "SELECT c.car_id, c.car_title, c.car_type, c.seat_capacity, c.amount, d.file_link from Car c JOIN \"DOCUMENT\" d ON c.car_Id = d.car_id WHERE d.document_type = 'car_image'";
     $stid = $db->executeQuery($sql);
     $result = $db->fetchAll($stid);
     if ($result && count($result) > 0) {
@@ -14,4 +14,25 @@ function getAllCars($db)
     return false;
   }
 }
+
+function getCarDetails($carId, $db)
+{
+  try {
+    $sql = "SELECT c.*, d.file_link from Car c JOIN \"DOCUMENT\" d ON c.car_Id = d.car_id WHERE d.document_type = 'car_image' AND c.car_id = :car_id";
+
+    $data = [":car_id" => $carId];
+
+    $stid = $db->executeQuery($sql, $data);
+    $result = $db->fetchRow($stid);
+    if ($result && count($result) > 0) {
+      return $result;
+    }
+    return [];
+  } catch (Exception $e) {
+    error_log($e->getMessage());
+    return false;
+  }
+}
+
+
 
