eb6fbc42bec42f3be1ca93c46b3a52b57490deb7
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2024-05-04 21:31:43 +0800

    improve create car

---
+++ functions/create-car.php
@@ -0,0 +1,103 @@
+<?php
+require_once "../utils/OracleDb.php";
+require_once "../utils/upload.php";
+if (session_status() == PHP_SESSION_NONE) {
+  session_start();
+}
+$owner_id = $_SESSION['user_id'];
+$db = new OracleDB();
+if (!$db->isConnected()) {
+  die("Database connection failed");
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (isset(
+    $_POST['car_title'],
+    $_POST['car_description'],
+    $_POST['car_model'],
+    $_POST['plate_number'],
+    $_POST['car_color'],
+    $_POST['car_brand'],
+    $_POST['gas_type'],
+    $_POST['seat_capacity'],
+    $_POST['car_type'],
+    $_POST['amount'],
+    $_FILES['orcr'],
+    $_FILES['car_image'],
+  )) {
+    try {
+      $car_title = $_POST['car_title'];
+      $car_description = $_POST['car_description'];
+      $car_model = $_POST['car_model'];
+      $plate_number = $_POST['plate_number'];
+      $car_color = $_POST['car_color'];
+      $gas_type = $_POST['gas_type'];
+      $car_brand = $_POST['car_brand'];
+      $seat_capacity = $_POST['seat_capacity'];
+      $car_type = $_POST['car_type'];
+      $amount = $_POST['amount'];
+      $orcr = $_FILES['orcr'];
+      $orcr = $_FILES['car_image'];
+
+      $sql = "INSERT INTO Car (car_id, car_title, car_description, car_model, plate_number, car_color, car_brand, availability_status, status, gas_type, seat_capacity, car_type, amount, owner_id) VALUES (car_seq.NEXTVAL, :car_title, :car_description, :car_model, :plate_number, :car_color, :car_brand, :availability_status, :status, :gas_type, :seat_capacity, :car_type, :amount, :owner_id) RETURNING car_id INTO :new_car_id";
+
+      $data = [
+        ':car_title' => $car_title,
+        ':car_description' => $car_description,
+        ':car_model' => $car_model,
+        ':plate_number' => $plate_number,
+        ':car_color' => $car_color,
+        ':car_brand' => $car_brand,
+        ':seat_capacity' => $seat_capacity,
+        ':gas_type' => $gas_type,
+        ':car_type' => $car_type,
+        ':amount' => $amount,
+        ':availability_status' => 0,
+        ':status' => 0,
+        ':owner_id' => $owner_id,
+      ];
+
+      $stid = $db->prepareStatement($sql);
+      foreach ($data as $key => $val) {
+        if (!oci_bind_by_name($stid, $key, $data[$key])) {
+          $e = oci_error($stid);
+          throw new Exception('Bind Error: ' . $e['message']);
+        }
+      }
+
+      $new_car_id = 0;
+      oci_bind_by_name($stid, ":new_car_id", $new_car_id, -1, OCI_B_INT);
+      if (!oci_execute($stid)) {
+        $e = oci_error($stid);
+        throw new Exception('Execute Error: ' . $e['message']);
+      }
+      echo "<p>Car details saved successfully!</p>";
+
+      $documents = [
+        'orcr' => 'ORCR',
+        'car_image' => 'Car Image',
+      ];
+
+      $allFilesProvided = true;
+      foreach ($documents as $inputName => $docType) {
+        if (!isset($_FILES[$inputName]) || $_FILES[$inputName]['error'] ==         UPLOAD_ERR_NO_FILE) {
+          echo "File $docType not provided.<br>";
+          $allFilesProvided = false;
+        }
+      }
+      if ($allFilesProvided) {
+        foreach ($documents as $inputName => $documentName) {
+          uploadCarDocuments($_FILES[$inputName], $new_car_id, $inputName, $documentName, $db);
+        }
+      } else {
+        echo "All documents are required. Please upload each file.<br>";
+      }
+      oci_free_statement($stid);
+      header("Location: /drivesation/lease-car.php");
+    } catch (Exception $e) {
+      throw new Exception("EROR", $e->getMessage());
+    }
+  } else {
+    throw new Exception("Input required fields");
+  }
+}
